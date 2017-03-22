0caf526773ad6c557b5cf15b0cb5206deae3cfac
Barton Chittenden <barton@bywatersolutions.com>
2017-03-22 16:06:43 -0400

    Fix syntax errror in item_bad_onloan_date.sql

--- item_bad_onloan_date.sql
+++ item_bad_onloan_date.sql
@@ -1,17 +1,17 @@
 SELECT
     items.itemnumber,
-    items.biblionumber, 
-    biblio.title, 
-    items.onloan
-    items.dateaccessioned, 
-    items.replacementpricedate, 
-    items.datelastborrowed, 
-    items.datelastseen, 
-    items.itemlost_on, 
-    items.withdrawn_on, 
-    items.timestamp, 
+    items.biblionumber,
+    biblio.title,
+    items.onloan,
+    items.dateaccessioned,
+    items.replacementpricedate,
+    items.datelastborrowed,
+    items.datelastseen,
+    items.itemlost_on,
+    items.withdrawn_on,
+    items.timestamp
 FROM
-    items 
+    items
     INNER JOIN biblio USING (biblionumber)
 WHERE
     DATE( items.onloan ) = '0000-00-00'
